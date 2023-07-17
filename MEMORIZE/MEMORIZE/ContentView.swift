80cd4d8d5383cfa761f5451462d0247c73b06f9f
Maurício Mendonça <mauricio.mendonca13@outlook.com>
2023-07-17 19:09:49 -0300

    Commit Buttons and array

--- MEMORIZE/MEMORIZE/ContentView.swift
+++ MEMORIZE/MEMORIZE/ContentView.swift
@@ -8,19 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis: Array<String> = ["🚎","🚂", "🚢", "🚀",  "🛸",  "🚘",  "🚍",  "🚚",  "🚖",  "🏎️",  "🚛",  "✈️",  "🚠"]
+    @State var emojiCount = 6
+    
     var body: some View {
-        HStack{
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        VStack{
+            HStack{
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(content: emoji)
+                }
+            }
+            HStack {
+                Button(action: {
+                    emojiCount += 1
+                }, label: { VStack{
+                    Text("Add")
+                    Text("Card")
+                }})
+                Spacer()
+                Button(action: {
+                    emojiCount -= 1
+                }, label: {VStack{
+                    Text("Remove")
+                    Text("Card")
+                }})
+            }
+            .padding(.horizontal)
         }
-        .foregroundColor(.red)
-   
+            .foregroundColor(.red)
     }
 }
 
 struct CardView: View {
+    var content: String
     @State var isFaceUp: Bool = true
     var body: some View{
         return ZStack {
@@ -29,7 +49,7 @@ struct CardView: View {
             {
                 shape.fill().foregroundColor(.white)
                 shape.stroke(lineWidth: 3)
-                Text("✈️").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 shape.fill()
             }
